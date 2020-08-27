96900c784810bcb06eb06c7137d00e1fa0af599e
Taylor Sieling <taylorasieling@gmail.com>
2020-08-27 01:18:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
-  if array.length == 2 
+  if array.length == 1
+    ""
+  elsif array.length == 2 
     "#{array[0]} and #{array[1]}"
-  elsif array.length >= 3 
+  else array.length >= 3 
     array[-1].insert(0, "and ")
     array.join(", ")
   end
