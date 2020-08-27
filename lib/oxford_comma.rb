47ea495d10d50276dbab5b5edfd631d60a44f54d
Taylor Sieling <taylorasieling@gmail.com>
2020-08-27 00:48:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
-def oxford_comma(array)
-  if array.size == 2 
-    return "#{array[0]} and #{array[1]}"
-  else array.length >= 3
-   puts array.join(" , ")
+# oxford_comma(array)
+ # if array.size == 2 
+  #  return "#{array[0]} and #{array[1]}"
+  #else array.length >= 3
+  # puts array.join(" , ")
  end
 end
 
