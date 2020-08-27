f1cc77ac916aab5595e3da3e133dc23900118d60
Taylor Sieling <taylorasieling@gmail.com>
2020-08-27 01:20:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2 
+    "#{array[0]} and #{array[1]}"
+  else array.length >= 3 
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  end
 end
