1519c021e8b30aafebadf7743e964c4100537438
Taylor Sieling <taylorasieling@gmail.com>
2020-08-27 00:49:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,1 @@
-# oxford_comma(array)
- # if array.size == 2 
-  #  return "#{array[0]} and #{array[1]}"
-  #else array.length >= 3
-  # puts array.join(" , ")
-# end
-#end
-
-def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
-  end
-  array.join(", ")    
-end
+def oxf
